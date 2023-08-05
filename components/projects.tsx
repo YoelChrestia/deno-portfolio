004d287a792b0388752b4d4f846c7435b6b733b8
YoelChrestia <yoel.c@knownonline.com>
2023-08-05 19:19:28 -0300

    projects component

---
+++ components/projects.tsx
@@ -0,0 +1,58 @@
+const Projects = () => (
+  <div class="mt-5">
+    <h3 class="text-2xl font-medium text-almostwhite">
+      Proyectos
+    </h3>
+
+    <div class="grid grid-flow-row grid-cols-1 gap-4 mt-3 w-auto">
+      <div class="flex justify-start gap-4">
+        <div class="flex items-center justify-center">
+          <div class=" bg-accent w-9 h-9">
+          </div>
+        </div>
+        <div>
+          <a href="/">
+            <h4 class="text-accent lg:hover:underline">Lorem</h4>
+          </a>
+          <span class="text-secundary">
+            Lorem ipsum dolor sit amet consectetur.
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-flow-row grid-cols-1 gap-4 mt-3 w-auto">
+      <div class="flex justify-start gap-4">
+        <div class="flex items-center justify-center">
+          <div class=" bg-accent w-9 h-9">
+          </div>
+        </div>
+        <div>
+          <a href="/">
+            <h4 class="text-accent lg:hover:underline">Lorem</h4>
+          </a>
+          <span class="text-secundary">
+            Lorem ipsum dolor sit amet consectetur.
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-flow-row grid-cols-1 gap-4 mt-3 w-auto">
+      <div class="flex justify-start gap-4">
+        <div class="flex items-center justify-center">
+          <div class=" bg-accent w-9 h-9">
+          </div>
+        </div>
+        <div>
+          <a href="/">
+            <h4 class="text-accent lg:hover:underline">Lorem</h4>
+          </a>
+          <span class="text-secundary">
+            Lorem ipsum dolor sit amet consectetur.
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Projects;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,18 @@
 import Experience from "../components/experience.tsx";
 import Knowledge from "../components/knowledge.tsx";
 import Presentation from "../components/presentation.tsx";
+import Projects from "../components/projects.tsx";
 
 export default function Home() {
   return (
     <>
       <main class="bg-primary px-3 py-5">
-        <Presentation />
-        <Experience />
-        <Knowledge />
+        <div class="m-auto flex flex-col max-w-4xl">
+          <Presentation />
+          <Experience />
+          <Knowledge />
+          <Projects />
+        </div>
       </main>
     </>
   );
