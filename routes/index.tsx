d60a6cd1f6775dddd646eeb2e02b1c0ddb561556
YoelChrestia <yoel.c@knownonline.com>
2023-08-04 23:39:01 -0300

    Initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 class="text-xl p-2">Deno portfolio</h1>
+      </main>
+    </>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./routes/**/*.{tsx,ts}",
+    "./islands/**/*.{tsx,ts}",
+    "./components/**/*.{tsx,ts}",
+  ],
+  theme: {},
+  plugins: [],
+};
