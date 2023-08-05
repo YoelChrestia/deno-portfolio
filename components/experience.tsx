19d151df99db7b13b0a6aef76803d1de8771dda5
YoelChrestia <yoel.c@knownonline.com>
2023-08-05 17:46:16 -0300

    experience component

---
+++ components/experience.tsx
@@ -0,0 +1,39 @@
+const Experience = () => (
+  <div class="mt-8">
+    <h3 class="text-2xl font-medium text-almostwhite">
+      Experiencia
+    </h3>
+
+    <span class="flex text-sm text-secundary mt-5">2020 - 2020</span>
+    <h4 class="text-2xl font-medium text-accent">
+      Lorem | Front-end developer
+    </h4>
+    <p class="text-secundary mt-1">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde earum ipsa
+      consectetur? Molestias officiis ab doloribus nostrum fugit at aspernatur.
+      Aut explicabo quo ad molestias, impedit itaque labore doloribus facilis.
+    </p>
+
+    <span class="flex text-sm text-secundary mt-5">2020 - 2020</span>
+    <h4 class="text-2xl font-medium text-accent">
+      Lorem | Front-end developer
+    </h4>
+    <p class="text-secundary mt-1">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde earum ipsa
+      consectetur? Molestias officiis ab doloribus nostrum fugit at aspernatur.
+      Aut explicabo quo ad molestias, impedit itaque labore doloribus facilis.
+    </p>
+
+    <span class="flex text-sm text-secundary mt-5">2020 - 2020</span>
+    <h4 class="text-2xl font-medium text-accent">
+      Lorem | Front-end developer
+    </h4>
+    <p class="text-secundary mt-1">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde earum ipsa
+      consectetur? Molestias officiis ab doloribus nostrum fugit at aspernatur.
+      Aut explicabo quo ad molestias, impedit itaque labore doloribus facilis.
+    </p>
+  </div>
+);
+
+export default Experience;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+import Experience from "../components/experience.tsx";
 import Presentation from "../components/presentation.tsx";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <main class="bg-primary px-3 py-5">
         <Presentation />
+        <Experience />
       </main>
     </>
   );
