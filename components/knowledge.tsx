ced871537165238c047abc5b3d12962fbdb76bf9
YoelChrestia <yoel.c@knownonline.com>
2023-08-05 18:07:44 -0300

    knownledge component

---
+++ components/knowledge.tsx
@@ -0,0 +1,39 @@
+const Knowledge = () => (
+  <div class="mt-8">
+    <h3 class="text-2xl font-medium text-almostwhite">
+      Conocimientos
+    </h3>
+
+    <h4 class="text-2xl font-medium text-accent mt-5">
+      Lorem
+    </h4>
+    <div class="grid text-secundary grid-flow-row grid-cols-2 gap-3 mt-3">
+      <span>javascript</span>
+      <span>react</span>
+      <span>java</span>
+      <span>python</span>
+    </div>
+
+    <h4 class="text-2xl font-medium text-accent mt-5">
+      Lorem
+    </h4>
+    <div class="grid text-secundary grid-flow-row grid-cols-2 gap-3 mt-3">
+      <span>javascript</span>
+      <span>react</span>
+      <span>java</span>
+    </div>
+
+    <h4 class="text-2xl font-medium text-accent mt-5">
+      Lorem
+    </h4>
+    <div class="grid text-secundary grid-flow-row grid-cols-2 gap-3 mt-3">
+      <span>javascript</span>
+      <span>react</span>
+      <span>java</span>
+      <span>python</span>
+      <span>java</span>
+    </div>
+  </div>
+);
+
+export default Knowledge;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 import Experience from "../components/experience.tsx";
+import Knowledge from "../components/knowledge.tsx";
 import Presentation from "../components/presentation.tsx";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <main class="bg-primary px-3 py-5">
         <Presentation />
         <Experience />
+        <Knowledge />
       </main>
     </>
   );
